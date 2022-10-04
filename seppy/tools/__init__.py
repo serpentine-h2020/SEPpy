1872986565729193b19dcabbdf83dfa99d3aa05a
jgieseler <jan.gieseler@utu.fi>
2022-10-04 11:36:05 +0300

    add inf_inj_time() function to tools

--- seppy/tools/__init__.py
+++ seppy/tools/__init__.py
@@ -9,10 +9,12 @@ import numpy as np
 import pandas as pd
 import astropy.units as u
 import astropy.constants as const
+import sunpy.sun.constants as sconst
 from sunpy.coordinates import get_horizons_coord
 from matplotlib.dates import DateFormatter
 from matplotlib.ticker import ScalarFormatter
 from matplotlib.offsetbox import AnchoredText
+from numpy import sqrt, log, pi
 from pandas.tseries.frequencies import to_offset
 from seppy.loader.psp import calc_av_en_flux_PSP_EPIHI, calc_av_en_flux_PSP_EPILO, psp_isois_load
 from seppy.loader.soho import calc_av_en_flux_ERNE, soho_load
@@ -2046,3 +2048,114 @@ def calc_av_en_flux_sixs(df, channel, species):
             en_channel_string = ''
 
     return flux, en_channel_string
+
+
+"""
+inf_inj_time.py
+"""
+SOLAR_ROT = sconst.get('sidereal rotation rate').to(u.rad/u.s)
+
+
+def get_sun_coords(time='now'):
+    '''
+    Gets the astropy Sun coordinates.
+
+    Args:
+        time (datetime.datetime): time at which coordinates are fetched.
+
+    Returns:
+        sun coordinates.
+    '''
+
+    return get_horizons_coord("Sun", time=time)
+
+
+def radial_distance_to_sun(spacecraft, time='now'):
+    '''
+    Gets the 3D radial distance of a spacecraft to the Sun.
+    3D here means that it's the real spatial distance and not
+    a projection on, say, the solar equatorial plane.
+
+    Args:
+        spacecraft (str): spacecraft to look for.
+        time (datetime.datetime): time at which to look for.
+
+    Returns:
+        astropy units: radial distance.
+    '''
+
+    sc_coords = get_horizons_coord(spacecraft, time)
+
+    return sc_coords.separation_3d(get_sun_coords(time=time))
+
+
+def calc_spiral_length(radial_dist, sw_speed):
+    '''
+    Calculates the Parker spiral length from the Sun up to a given radial distance.
+
+    Args:
+        radial_dist (astropy units): radial distance to the Sun.
+        sw_speed (astropy units): solar wind speed.
+
+    Returns:
+        astropy units: Parker spiral length.
+    '''
+
+    temp_const = ((SOLAR_ROT/sw_speed)*(radial_dist.to(u.km)-const.R_sun)).value
+    sqrt_temp_const = sqrt(temp_const**2 + 1)
+
+    return 0.5*u.rad * (sw_speed/SOLAR_ROT) * (temp_const*sqrt_temp_const + log(temp_const + sqrt_temp_const))
+
+
+def calc_particle_speed(mass, kinetic_energy):
+    '''
+    Calculates the relativistic particle speed.
+
+    Args:
+        mass (astropy units): mass of the particle.
+        kinetic_energy (astropy units): kinetic energy of the particle.
+
+    Returns:
+        astropy units: relativistic particle speed.
+    '''
+
+    gamma = sqrt(1 - (mass*const.c**2/(kinetic_energy + mass*const.c**2))**2)
+
+    return gamma*const.c
+
+
+def inf_inj_time(spacecraft, onset_time, species, kinetic_energy, sw_speed):
+    '''
+    Calculates the inferred injection time of a particle (electron or proton) from the Sun,
+    given a detection time at some spacecraft.
+
+    Args:
+        spacecraft (str): name of the spacecraft.
+        onset_time (datetime.datetime): time of onset/detection.
+        species (str): particle species, 'p' or 'e'.
+        kinetic_energy (astropy units): kinetic energy of particle. If no unit is supplied, is converted to MeV.
+        sw_speed (astropy units): solar wind speed. If no unit is supplied, is converted to km/s.
+
+    Returns:
+        datetime.datetime: inferred injection time.
+    '''
+
+    if not type(kinetic_energy)==u.quantity.Quantity:
+        kinetic_energy = kinetic_energy * u.MeV
+
+    if not type(sw_speed)==u.quantity.Quantity:
+        sw_speed = sw_speed * u.km/u.s
+
+    mass_dict = {'p': const.m_p,
+                 'e': const.m_e
+                 }
+
+    radial_distance = radial_distance_to_sun(spacecraft, time=onset_time)
+
+    spiral_length = calc_spiral_length(radial_distance, sw_speed)
+    particle_speed = calc_particle_speed(mass_dict[species], kinetic_energy)
+
+    travel_time = spiral_length/particle_speed
+    travel_time = travel_time.to(u.s)
+
+    return onset_time - datetime.timedelta(seconds=travel_time.value), spiral_length.to(u.AU)
