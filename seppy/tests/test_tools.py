2720c6823e36bd6e0a47eb951c3f25041b20ce7c
Jan Gieseler <jan.gieseler@utu.fi>
2024-02-02 17:04:53 +0200

    change timestamp handling of tests

--- seppy/tests/test_tools.py
+++ seppy/tests/test_tools.py
@@ -27,9 +27,9 @@ def test_onset_SOLO_STEP_ions_old_data_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2020-09-21 17:27:32.010263')
+    assert onset_stats[5].isoformat().split('.')[0] == '2020-09-21T17:27:32'
     assert onset_found
-    assert peak_time == pd.Timestamp('2020-09-21 17:57:32.010263')
+    assert peak_time.isoformat().split('.')[0] == '2020-09-21T17:57:32'
     assert fig.get_axes()[0].get_title() == 'SOLO/STEP 0.0060 - 0.0091 MeV/n protons\n5min averaging, viewing: PIXEL AVERAGED'
     # Pixel 8 - check that calculation is stopped bc. this data is not implemented correctly!
     check = False
@@ -53,18 +53,18 @@ def test_onset_SOLO_STEP_ions_new_data_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:07:30.153419')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:07:30'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 18:57:30.153419')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T18:57:30'
     assert fig.get_axes()[0].get_title() == 'SOLO/STEP 0.0061 - 0.0091 MeV protons\n5min averaging, viewing: PIXEL AVERAGED'
     # Pixel 8
     flux, onset_stats, onset_found, peak_flux, peak_time, fig, bg_mean = Event1.find_onset(viewing='Pixel 8', background_range=background_range, channels=1, resample_period="5min", yscale='log', cusum_window=30)
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:12:30.153419')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:12:30'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 20:52:30.153419')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T20:52:30'
     assert fig.get_axes()[0].get_title() == 'SOLO/STEP 0.0061 - 0.0091 MeV protons\n5min averaging, viewing: PIXEL 8'
 
 
@@ -82,7 +82,7 @@ def test_onset_SOLO_HET_online():
     assert len(onset_stats) == 6
     assert isinstance(onset_stats[5], pd._libs.tslibs.nattype.NaTType)  # onset_stats[5] == pd.Timestamp('2021-10-28 15:31:59.492059')
     assert ~onset_found
-    assert peak_time == pd.Timestamp('2022-11-08 17:57:54.269660')
+    assert peak_time.isoformat().split('.')[0] == '2022-11-08T17:57:54'
     assert fig.get_axes()[0].get_title() == 'SOLO/HET 7.3540 - 7.8900 MeV protons\n5min averaging, viewing: SUN'
     # viewing "north", combined channel, electrons
     Event1 = Event(spacecraft='Solar Orbiter', sensor='HET', data_level='l2', species='electrons', start_date=startdate, end_date=enddate, data_path=lpath)
@@ -93,7 +93,7 @@ def test_onset_SOLO_HET_online():
     assert len(onset_stats) == 6
     assert isinstance(onset_stats[5], pd._libs.tslibs.nattype.NaTType)  # onset_stats[5] == pd.Timestamp('2021-10-28 15:31:59.492059')
     assert ~onset_found
-    assert peak_time == pd.Timestamp('2022-11-08 22:27:54.269660')
+    assert peak_time.isoformat().split('.')[0] == '2022-11-08T22:27:54'
     assert fig.get_axes()[0].get_title() == 'SOLO/HET 0.4533 - 18.8300 MeV electrons\n5min averaging, viewing: NORTH'
 
 
@@ -111,7 +111,7 @@ def test_onset_SOLO_EPT_online():
     assert len(onset_stats) == 6
     assert isinstance(onset_stats[5], pd._libs.tslibs.nattype.NaTType)  # onset_stats[5] == pd.Timestamp('2021-10-28 15:31:59.492059')
     assert ~onset_found
-    assert peak_time == pd.Timestamp('2022-06-06 01:02:30.902854')
+    assert peak_time.isoformat().split('.')[0] == '2022-06-06T01:02:30'
     assert fig.get_axes()[0].get_title() == 'SOLO/EPT 0.0608 - 0.0678 MeV protons\n5min averaging, viewing: SUN'
     # viewing "north", combined channel, electrons
     Event1 = Event(spacecraft='Solar Orbiter', sensor='EPT', data_level='l2', species='electrons', start_date=startdate, end_date=enddate, data_path=lpath)
@@ -122,7 +122,7 @@ def test_onset_SOLO_EPT_online():
     assert len(onset_stats) == 6
     assert isinstance(onset_stats[5], pd._libs.tslibs.nattype.NaTType)  # onset_stats[5] == pd.Timestamp('2021-10-28 15:31:59.492059')
     assert ~onset_found
-    assert peak_time == pd.Timestamp('2022-06-06 23:02:30.902854')
+    assert peak_time.isoformat().split('.')[0] == '2022-06-06T23:02:30'
     assert fig.get_axes()[0].get_title() == 'SOLO/EPT 0.0334 - 0.0439 MeV electrons\n5min averaging, viewing: NORTH'
 
 
@@ -138,9 +138,9 @@ def test_onset_PSP_ISOIS_EPIHI_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (194,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 15:31:59.492059')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T15:31:59'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 16:06:59.492059')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T16:06:59'
     assert fig.get_axes()[0].get_title() == 'PSP/ISOIS-EPIHI 0.8 - 1.0 MeV electrons\n5min averaging, viewing: A'
     # viewing "B", combined channel, protons
     Event1 = Event(spacecraft='PSP', sensor='isois-epihi', data_level='l2', species='protons', start_date=startdate, end_date=enddate, data_path=lpath)
@@ -149,9 +149,9 @@ def test_onset_PSP_ISOIS_EPIHI_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (194,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:46:59.492059')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:46:59'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 19:56:59.492059')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T19:56:59'
     assert fig.get_axes()[0].get_title() == 'PSP/ISOIS-EPIHI 8.0 - 19.0 MeV protons\n5min averaging, viewing: B'
 
 
@@ -167,18 +167,18 @@ def test_onset_PSP_ISOIS_EPILO_e_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (198,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 15:33:14.991967')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T15:33:14'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 16:43:14.991967')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T16:43:14'
     assert fig.get_axes()[0].get_title() == 'PSP/ISOIS-EPILO 65.9 - 100.5 keV electrons\n5min averaging, viewing: 7'
     # viewing "3", combined channels
     flux, onset_stats, onset_found, peak_flux, peak_time, fig, bg_mean = Event1.find_onset(viewing='3', background_range=background_range, channels=[0, 4], resample_period="5min", yscale='log', cusum_window=30)
     assert isinstance(flux, pd.Series)
     assert flux.shape == (198,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 15:33:14.991967')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T15:33:14'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 17:48:14.991967')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T17:48:14'
     assert fig.get_axes()[0].get_title() == 'PSP/ISOIS-EPILO 10.0 - 100.5 keV electrons\n5min averaging, viewing: 3'
 
 
@@ -194,18 +194,18 @@ def test_onset_Wind_3DP_p_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:27:35.959000')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:27:35'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 20:17:35.959000')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T20:17:35'
     assert fig.get_axes()[0].get_title() == 'WIND/3DP 385.96 - 716.78 keV protons\n5min averaging, viewing: SECTOR 3'
     # viewing "omnidirectional"
     flux, onset_stats, onset_found, peak_flux, peak_time, fig, bg_mean = Event1.find_onset(viewing='omnidirectional', background_range=background_range, channels=4, resample_period="5min", yscale='log', cusum_window=30)
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:27:42.224000')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:27:42'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 21:07:42.224000')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T21:07:42'
     assert fig.get_axes()[0].get_title() == 'WIND/3DP 385.96 - 716.78 keV protons\n5min averaging, viewing: OMNIDIRECTIONAL'
     # no channel combination inlcuded for Wind/3DP, yet
 
@@ -222,18 +222,18 @@ def test_onset_Wind_3DP_e_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:12:35.959000')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:12:35'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 21:52:35.959000')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T21:52:35'
     assert fig.get_axes()[0].get_title() == 'WIND/3DP 127.06 - 235.96 keV electrons\n5min averaging, viewing: SECTOR 3'
     #
     flux, onset_stats, onset_found, peak_flux, peak_time, fig, bg_mean = Event1.find_onset(viewing='omnidirectional', background_range=background_range, channels=4, resample_period="5min", yscale='log', cusum_window=30)
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:07:42.224000')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:07:42'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 21:52:42.224000')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T21:52:42'
     assert fig.get_axes()[0].get_title() == 'WIND/3DP 127.06 - 235.96 keV electrons\n5min averaging, viewing: OMNIDIRECTIONAL'
     # no channel combination inlcuded for Wind/3DP, yet
 
@@ -250,9 +250,9 @@ def test_onset_STEREOB_HET_p_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2006-12-13 03:18:04')
+    assert onset_stats[5].isoformat().split('.')[0] == '2006-12-13T03:18:04'
     assert onset_found
-    assert peak_time == pd.Timestamp('2006-12-13 09:53:04')
+    assert peak_time.isoformat().split('.')[0] == '2006-12-13T09:53:04'
     assert fig.get_axes()[0].get_title() == 'STB/HET 26.3 - 40.5 MeV protons\n5min averaging'
 
 
@@ -268,9 +268,9 @@ def test_onset_STEREOB_HET_e_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2006-12-13 02:43:04')
+    assert onset_stats[5].isoformat().split('.')[0] == '2006-12-13T02:43:04'
     assert onset_found
-    assert peak_time == pd.Timestamp('2006-12-13 04:53:04')
+    assert peak_time.isoformat().split('.')[0] == '2006-12-13T04:53:04'
     assert fig.get_axes()[0].get_title() == 'STB/HET 1.4 - 2.8 MeV electrons\n5min averaging'
 
 
@@ -286,9 +286,9 @@ def test_onset_STEREOA_SEPT_p_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 15:53:27.974418944')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T15:53:27'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 17:18:27.974418944')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T17:18:27'
     assert fig.get_axes()[0].get_title() == 'STA/SEPT 110-174.6 keV protons\n5min averaging, viewing: NORTH'
 
 
@@ -304,9 +304,9 @@ def test_onset_STEREOA_SEPT_e_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 15:43:27.974418944')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T15:43:27'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 18:58:27.974418944')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T18:58:27'
     assert fig.get_axes()[0].get_title() == 'STA/SEPT 125-145 keV electrons\n5min averaging, viewing: ASUN'
 
 
@@ -322,9 +322,9 @@ def test_onset_SOHO_EPHIN_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 15:53:42.357000')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T15:53:42'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 22:18:42.357000')  # pd.Timestamp('2021-10-29 04:53:42.357000')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T22:18:42'  # '2021-10-29T04:53:42.357000')
     assert fig.get_axes()[0].get_title() == 'SOHO/EPHIN 0.25 - 0.7 MeV electrons\n5min averaging'
     # no channel combination inlcuded for SOHO/EPHIN electrons, yet
 
@@ -341,9 +341,9 @@ def test_onset_SOHO_ERNE_online():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:53:05.357000')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:53:05'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 22:53:05.357000')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T22:53:05'
     assert fig.get_axes()[0].get_title() == 'SOHO/ERNE 25.0 - 32.0 MeV protons\n5min averaging'
 
 
@@ -360,9 +360,9 @@ def test_onset_SOHO_ERNE_offline():
     assert isinstance(flux, pd.Series)
     assert flux.shape == (288,)
     assert len(onset_stats) == 6
-    assert onset_stats[5] == pd.Timestamp('2021-10-28 16:53:05.357000')
+    assert onset_stats[5].isoformat().split('.')[0] == '2021-10-28T16:53:05'
     assert onset_found
-    assert peak_time == pd.Timestamp('2021-10-28 22:53:05.357000')
+    assert peak_time.isoformat().split('.')[0] == '2021-10-28T22:53:05'
     assert fig.get_axes()[0].get_title() == 'SOHO/ERNE 16.0 - 32.0 MeV protons\n5min averaging'
 
 
