b42938d32e877f4ecaafa92342e84037235a61f2
Jan Gieseler <jan.gieseler@utu.fi>
2025-08-07 11:39:42 +0300

    add channels_dict

--- seppy/loader/bepi.py
+++ seppy/loader/bepi.py
@@ -56,7 +56,7 @@ def bepi_sixsp_download(date, path=None):
     return downloaded_file
 
 
-def bepi_sixsp_loader(startdate, enddate, resample=None, path=None, pos_timestamp='center'):
+def bepi_sixsp_l3_loader(startdate, enddate, resample=None, path=None, pos_timestamp='center'):
     """Loads BepiColombo/SIXS-P level 3 data and returns it as Pandas dataframe together with a dictionary providing the energy ranges per channel
 
     Parameters
@@ -75,36 +75,40 @@ def bepi_sixsp_loader(startdate, enddate, resample=None, path=None, pos_timestam
     Returns
     -------
     df : Pandas dataframe
-        dataframe with either 15 channels of electron or 30 channels of proton/ion fluxes and their respective uncertainties
+        Pandas dataframe of measured fluxes and uncertaintites
     channels_dict_df : dict
-        Pandas dataframe giving details on the measurement channels
+        Dictionary giving details on the measurement channels
     """
 
-
-
-    # <--------------------
-    # channel dicts:
-    ch_strings = ['45.0-55.0 keV', '55.0-65.0 keV', '65.0-75.0 keV', '75.0-85.0 keV', '85.0-105.0 keV', '105.0-125.0 keV', '125.0-145.0 keV', '145.0-165.0 keV', '165.0-195.0 keV', '195.0-225.0 keV', '225.0-255.0 keV', '255.0-295.0 keV', '295.0-335.0 keV', '335.0-375.0 keV', '375.0-425.0 keV']
-    mean_E = []
-    for i in range(len(ch_strings)):
-        temp  = ch_strings[i].split(' keV')
-        clims = temp[0].split('-')
-        lower = float(clims[0])
-        upper = float(clims[1])
-        mean_E.append(np.sqrt(upper*lower))
-
-    echannels = {'bins': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-                 'ch_strings': ch_strings,
-                 'DE': [0.0100, 0.0100, 0.0100, 0.0100, 0.0200, 0.0200, 0.0200, 0.0200, 0.0300, 0.0300, 0.0300, 0.0400, 0.0400, 0.0400, 0.0500],
-                 'mean_E': mean_E}
-
-    # create Pandas Dataframe from channels_dict:
-    channels_dict_df = pd.DataFrame.from_dict(echannels)
-    channels_dict_df.index = channels_dict_df.bins
-    channels_dict_df.drop(columns=['bins'], inplace=True)
-    # ----------------------->
-
-
+    # TODO:
+    # 1. verify values for Low and High energies
+    # 2. update PE low and high energies with real numbers
+    channels_dict = {"Side0_Energy_Bin_str": {'E1': '71 keV', 'E2': '106 keV', 'E3': '169 keV', 'E4': '278 keV', 'E5': '918 keV', 'E6': '2.12 MeV', 'E7': '7.01 MeV',
+                                              'P1': '1.1 MeV', 'P2': '1.2 MeV', 'P3': '1.5 MeV', 'P4': '2.3 MeV', 'P5': '3.9 MeV', 'P6': '8.0 MeV', 'P7': '14.3 MeV', 'P8': '25.4 MeV', 'P9': '49.8 MeV',
+                                              'PE1': '0.47 MeV', 'PE2': '0.82 MeV', 'PE3': '2.22 MeV'},
+                    "Side0_Electron_Bins_Effective_Energy": np.array([0.0714, 0.106, 0.169, 0.278, 0.918, 2.120, 7.010]),
+                    "Side0_Electron_Bins_Low_Energy": np.array([0.055, 0.078, 0.134, 0.235, 1.000, 1.432, 4.904]),
+                    "Side0_Electron_Bins_High_Energy": np.array([0.092, 0.143, 0.214, 0.331, 1.193, 3.165, 10.000]),
+                    "Side0_Proton_Bins_Effective_Energy": np.array([1.09, 1.19, 1.51, 2.26, 3.94, 8.02, 14.3, 25.4, 49.8]),
+                    "Side0_Proton_Bins_Low_Energy": np.array([0.001, 1.088, 1.407, 2.139, 3.647, 7.533, 13.211, 22.606, 29.246]),
+                    "Side0_Proton_Bins_High_Energy": np.array([1.254, 1.311, 1.608, 2.388, 4.241, 8.534, 15.515, 28.413, 40.0]),
+                    "Side0_Proton_As_Electron_Bins_Effective_Energy": np.array([0.468, 0.824, 2.22]),
+                    "Side0_Proton_As_Electron_Bins_Low_Energy": np.array([np.nan, np.nan, np.nan]),
+                    "Side0_Proton_As_Electron_Bins_High_Energy": np.array([np.nan, np.nan, np.nan])}
+    for i in range(1, 5):
+        channels_dict[f"Side{i}_Energy_Bin_str"] = {'E1': '73 keV', 'E2': '107 keV', 'E3': '168 keV', 'E4': '275 keV', 'E5': '918 keV', 'E6': '2.22 MeV', 'E7': '6.46 MeV',
+                                                    'P1': '1.1 MeV', 'P2': '1.2 MeV', 'P3': '1.5 MeV', 'P4': '2.3 MeV', 'P5': '3.9 MeV', 'P6': '8.0 MeV', 'P7': '14.5 MeV', 'P8': '25.1 MeV', 'P9': '49.8 MeV',
+                                                    'PE1': '0.46 MeV', 'PE2': '0.86 MeV', 'PE3': '2.47 MeV'}
+        channels_dict[f"Side{i}_Electron_Bins_Effective_Energy"] = np.array([0.0726, 0.107, 0.168, 0.275, 0.918, 2.220, 6.460])
+        channels_dict[f"Side{i}_Electron_Bins_Low_Energy"] = np.array([0.055, 0.078, 0.134, 0.235, 1.000, 1.432, 4.904])
+        channels_dict[f"Side{i}_Electron_Bins_High_Energy"] = np.array([0.092, 0.143, 0.214, 0.331, 1.193, 3.165, 10.000])
+        channels_dict[f"Side{i}_Proton_Bins_Effective_Energy"] = np.array([1.12, 1.22, 1.53, 2.28, 3.94, 8.02, 14.5, 25.1, 49.8])
+        channels_dict[f"Side{i}_Proton_Bins_Low_Energy"] = np.array([0.001, 1.088, 1.407, 2.139, 3.647, 7.533, 13.211, 22.606, 29.246])
+        channels_dict[f"Side{i}_Proton_Bins_High_Energy"] = np.array([1.254, 1.311, 1.608, 2.388, 4.241, 8.534, 15.515, 28.413, 40.0])
+        
+        channels_dict[f"Side{i}_Proton_As_Electron_Bins_Effective_Energy"] = np.array([0.459, 0.854, 2.47])
+        channels_dict[f"Side{i}_Proton_As_Electron_Bins_Low_Energy"] = np.array([np.nan, np.nan, np.nan])
+        channels_dict[f"Side{i}_Proton_As_Electron_Bins_High_Energy"] = np.array([np.nan, np.nan, np.nan])
 
 
     if not path:
@@ -151,4 +155,4 @@ def bepi_sixsp_loader(startdate, enddate, resample=None, path=None, pos_timestam
     else:
         df = []
 
-    return df, channels_dict_df
+    return df, channels_dict
